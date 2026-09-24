73593c492390ad35754d710043705d93e98b8931
BenjaminN. <benjaminmoreno0809@gmail.com>
2026-09-24 22:54:48 +0000

    arreglo

--- galeria_fotos/galeria_fotos/settings.py
+++ galeria_fotos/galeria_fotos/settings.py
@@ -130,3 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'foto_list'
 LOGOUT_REDIRECT_URL = 'foto_list'
+CSRF_TRUSTED_ORIGINS = ['https://*.app.github.dev']
